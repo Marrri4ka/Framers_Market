432d0bc22edd558168ead660cd0b3a628d55a137
Mariia <mariiapopovych@gmail.com>
2019-07-02 22:32:22 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FarmersMarket from './components/FarmersMarket';
+import {Switch, Route} from 'react-router-dom';
+import MarketScheduleList from './components/MarketScheduleList';
+import Header from './components/Header';
+import ProduceList from './components/ProduceList';
+import Home from './components/Home';
 
 function App() {
-  return (
-  <FarmersMarket/>
-  );
+  return (<div>
+    <Header/>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path="/marketSchedule" component={MarketScheduleList} />
+      <Route path="/produceList" component={ProduceList}/>
+    </Switch>
+
+  </div>);
 }
 
 export default App;
